0102b492df925093c89448842b798b7a9fc4e4d4
yuy.hirohiro@gmail.com <yuy.hirohiro@gmail.com>
2020-02-15 17:32:04 +0900

    usecaseのテスト作成

--- usecase/mock_test.go
+++ usecase/mock_test.go
@@ -1,4 +1,7 @@
 package usecase_test
 
+import "github.com/stretchr/testify/mock"
 
-
+type PodUsecaseMock struct {
+	mock.Mock
+}
--- usecase/pod_usecase_test.go
+++ usecase/pod_usecase_test.go
@@ -2,11 +2,42 @@ package usecase_test
 
 import (
 	"github.com/stretchr/testify/assert"
+	"kubetui/domain"
+	"kubetui/usecase"
 	"testing"
 )
 
 func Test指定したネームスペースのPodの一覧を取得することができる(t *testing.T) {
 
-	expected := []string{"sample-deployment-7bb5fc6bc6-9btmd","sample-deployment-7bb5fc6bc6-hhd2l"}
+	podGatewayMock := &PodUsecaseMock{}
+	podGatewayMock.On("FindPods", "").Return(
+		domain.Pods{
+			Pods: []domain.Pod{
+				{
+					Name:   "sample-deployment-7bb5fc6bc6-9btmd",
+					Ip:     "172.17.0.17",
+					Status: "Running",
+					Node:   "minikube",
+				},
+				{
+					Name:   "sample-deployment-7bb5fc6bc6-hhd2l",
+					Ip:     "172.17.0.19",
+					Status: "Running",
+					Node:   "minikube",
+				},
+			},
+		},
+	)
+	target := usecase.PodUsecase{PodPort: podGatewayMock}
+
+	expected := []string{"sample-deployment-7bb5fc6bc6-9btmd", "sample-deployment-7bb5fc6bc6-hhd2l"}
+
+	actual := target.GetPodList("")
+
 	assert.Equal(t, expected, actual)
+}
+
+func (p PodUsecaseMock) FindPods(namespace string) domain.Pods {
+	result := p.Called(namespace).Get(0)
+	return result.(domain.Pods)
 }
